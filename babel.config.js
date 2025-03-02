caa8db9b3592585e7c8375130aaba8830052ad80
Balasooriya R.C <ravindu.chamara2000@gmail.com>
2025-03-02 10:49:46 +0530

    fix not running error

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: [
-      'react-native-reanimated/plugin', // Required for react-native-reanimated
-    ],
-  };
-  
+  presets: ["babel-preset-expo"],
+  plugins: [
+    ["@babel/plugin-transform-class-properties", { "loose": true }],
+    ["@babel/plugin-transform-private-methods", { "loose": true }],
+    ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
+    "react-native-reanimated/plugin"
+  ],
+};
